68b8f68004b65f0897e03a0ea600ed7db372a4b2
James Cornall <jjc257@student.bham.ac.uk>
2023-08-30 14:00:51 +0100

    Add LogisticRegression and TF-IDF models

--- modelling.py
+++ modelling.py
@@ -16,20 +16,20 @@ import plotly.express as px
 import seaborn as sns
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
-import plotly   
+import plotly
 
 _JSON_INPUT_FOLDER = "E:\\Documents\\Education\\PG University of Birmingham\\MSc Computer Science\\Summer Semester\\MSc Projects\\Project Files\\Dataset\\final\\json"
 _JSON_TEST_FILE = "E:\\Documents\\Education\\PG University of Birmingham\\MSc Computer Science\\Summer Semester\\MSc Projects\\Project Files\\Dataset\\final\\json\\2013-01-08.json"
 
-## Preprocessing
+### Preprocessing
 
 with open(_JSON_TEST_FILE, "r") as file:
-    df = pd.read_json(file.read())
+    data = pd.read_json(file.read())
 
-df.replace({"aye" : 1, "no" : 0}, inplace = True)
+data.replace({"aye" : 1, "no" : 0}, inplace = True)
 
-edited_speech = df["text"].copy()
-df["speech_without_stopwords"] = edited_speech
+edited_speech = data["text"].copy()
+data["speech_without_stopwords"] = edited_speech
 
 def preprocess_speech_data(data, column):
     # Convert text to lowercase
@@ -45,8 +45,8 @@ def preprocess_speech_data(data, column):
     # Remove all single characters in the text
     data[column] = data[column].apply(lambda x:re.sub(r'\s+[a-zA-Z]\s+', "", x))  
     
-# preprocess_speech_data(df, "speech_without_stopwords")
-# print(df.head(5))
+# preprocess_speech_data(data, "speech_without_stopwords")
+# print(data.head(5))
     
 def remove_stopwords_tokenize(data, column):
     
@@ -67,8 +67,8 @@ def remove_stopwords_tokenize(data, column):
         x.append(getting_filter(i))
     data[column] = x
     
-# remove_stopwords_tokenize(df, "speech_without_stopwords")
-# print(df.head(5))
+# remove_stopwords_tokenize(data, "speech_without_stopwords")
+# print(data.head(5))
 
 lemmatizer = WordNetLemmatizer()
 
@@ -96,23 +96,23 @@ def lemmatize_data(data, column):
         x.append(getting_lemmatize(i))
     data[column] = x
     
-# lemmatize_data(df, "speech_without_stopwords")
-# print(df.head(5))
+# lemmatize_data(data, "speech_without_stopwords")
+# print(data.head(5))
 
 def join_paragraph(data, column):
     data[column] = data[column].apply(lambda x:" ".join([i + " " for i in x]))
     # Remove any double spaces
     data[column] = data[column].apply(lambda x:re.sub(r'\s+', " ", x, flags = re.I))
     
-preprocess_speech_data(df, "speech_without_stopwords")
-remove_stopwords_tokenize(df, "speech_without_stopwords")
-join_paragraph(df, "speech_without_stopwords")
+preprocess_speech_data(data, "speech_without_stopwords")
+remove_stopwords_tokenize(data, "speech_without_stopwords")
+join_paragraph(data, "speech_without_stopwords")
 
-final_data = df["speech_without_stopwords"].copy()
-df["post_lemmatization"] = final_data
+final_data = data["speech_without_stopwords"].copy()
+data["post_lemmatization"] = final_data
 
-lemmatize_data(df, "post_lemmatization")
-join_paragraph(df, "post_lemmatization")
+lemmatize_data(data, "post_lemmatization")
+join_paragraph(data, "post_lemmatization")
 
 pos = neg = obj = count = 0
 
@@ -122,11 +122,11 @@ pos_tagging = []
 
 nltk.download("averaged_perceptron_tagger", quiet = True)
 
-for speech in df["post_lemmatization"]:
+for speech in data["post_lemmatization"]:
     list = word_tokenize(speech)
     pos_tagging.append(nltk.pos_tag(list))
 
-df["pos_tags"] = pos_tagging
+data["pos_tags"] = pos_tagging
 
 def tags_to_wordnet(tag):
     if tag.startswith("J"):
@@ -168,7 +168,7 @@ def get_sentiment(word, tag):
     
 sentiment_score = []
 
-for pos_value in df["pos_tags"]:
+for pos_value in data["pos_tags"]:
     sentiment_value = [get_sentiment(x,y) for (x,y) in pos_value]
     for score in sentiment_value:
         try:
@@ -179,94 +179,68 @@ for pos_value in df["pos_tags"]:
     sentiment_score.append(pos - neg)
     pos = neg = 0    
     
-df["sentiment_score"] = sentiment_score
+data["sentiment_score"] = sentiment_score
 
 overall = []
-for i in range(len(df)):
-    if df["sentiment_score"][i] >= 0.05:
+for i in range(len(data)):
+    if data["sentiment_score"][i] >= 0.05:
         overall.append("Positive")
-    elif df["sentiment_score"][i] <= -0.05:
+    elif data["sentiment_score"][i] <= -0.05:
         overall.append("Negative")
     else:
         overall.append("Neutral")
-df["overall_sentiment"] = overall
-print(df.head(10))
+data["overall_sentiment"] = overall
 
 # Visualise the sentiment score results
 # sns.set_theme(style = "whitegrid")
-# sns.countplot(x=df["overall_sentiment"])
+# sns.countplot(x=data["overall_sentiment"])
 # plt.show() 
 
-df["speech_text_new"] = df["post_lemmatization"].copy()
+data["speech_text_new"] = data["post_lemmatization"].copy()
 
-## Modelling
+### Modelling
 
-# from sklearn.feature_extraction.text import CountVectorizer
+# Create a word-document matrix
+from sklearn.feature_extraction.text import CountVectorizer
 
-# vec = CountVectorizer()
-# X = vec.fit_transform(data['reviews_text_new'])
-# df = pd.DataFrame(X.toarray(), columns = vec.get_feature_names())
-# df.head(3)
+vect_word_doc = CountVectorizer()
+X = vect_word_doc.fit_transform(data["speech_text_new"])
+df = pd.DataFrame(X.toarray(), columns = vect_word_doc.get_feature_names_out())
 
-# vect = CountVectorizer()
-# vect.fit(data['reviews_text_new'])
-# vect.get_feature_names()
-# # transform training data into a 'document-term matrix'
-# simple_train_dtm = vect.transform(data['reviews_text_new'])
-# print(simple_train_dtm)
+# Transform training data into a 'document-term matrix'
+vect_doc_term = CountVectorizer()
+vect_doc_term.fit(data["speech_text_new"])
+vect_doc_term.get_feature_names_out()
+simple_train_dtm = vect_doc_term.transform(data["speech_text_new"])
+print(simple_train_dtm)
 
-# ### Creating a python object of the class CountVectorizer
-# from sklearn.feature_extraction.text import CountVectorizer
-# bow_counts = CountVectorizer(tokenizer= word_tokenize, # type of tokenization
-#                              ngram_range=(1,3)) # number of n-grams
+# Create CountVectorizer object for Bag of Words data
+bow_counts = CountVectorizer(tokenizer = word_tokenize, ngram_range=(1,3))
+bow_data = bow_counts.fit_transform(data["speech_text_new"])
 
-# bow_data = bow_counts.fit_transform(data['reviews_text_new'])
+# Divide data into training and test sets
+from sklearn.model_selection import train_test_split
+X_train_bow, X_test_bow, y_train_bow, y_test_bow = train_test_split(bow_data, data["overall_sentiment"], test_size = 0.2, random_state = 0)
 
-# from sklearn.model_selection import train_test_split
-# X_train_bow, X_test_bow, y_train_bow, y_test_bow = train_test_split(bow_data, # Features
-#                                                                     data['Overall Sentiment'], # Target variable
-#                                                                     test_size = 0.2, # 20% test size
-#                                                                     random_state = 0) # random
+# Train the LogisticRegression model
+from sklearn.linear_model import LogisticRegression
+lr_model_all = LogisticRegression()
+lr_model_all.fit(X_train_bow, y_train_bow)
+test_pred_lr_all = lr_model_all.predict(X_test_bow)
 
-# from sklearn.linear_model import LogisticRegression
-# ### Training the model 
-# lr_model_all = LogisticRegression() # Logistic regression
-# lr_model_all.fit(X_train_bow, y_train_bow) # Fitting a logistic regression model
+## Calculate performance metrics
+from sklearn.metrics import classification_report
+print(classification_report(y_test_bow,test_pred_lr_all))
 
-# ## Predicting the output
-# test_pred_lr_all = lr_model_all.predict(X_test_bow) # Class prediction
+# Apply TF-IDF model to data
+from sklearn.feature_extraction.text import TfidfVectorizer
+noise_words = []
+tfidf_counts = TfidfVectorizer(tokenizer= word_tokenize, stop_words=noise_words, ngram_range=(1,1))
+tfidf_data = tfidf_counts.fit_transform(data["speech_text_new"])
+X_train_tfidf, X_test_tfidf, y_train_tfidf, y_test_tfidf = train_test_split(tfidf_data, data["overall_sentiment"], test_size = 0.2, random_state = 0)
 
+lr_model_tf_idf = LogisticRegression()
+lr_model_tf_idf.fit(X_train_tfidf,y_train_tfidf)
+test_pred_lr_all = lr_model_tf_idf.predict(X_test_tfidf)
 
-# ## Calculate key performance metrics
-
-# from sklearn.metrics import classification_report
-# # Print a classification report
-# print(classification_report(y_test_bow,test_pred_lr_all))
-
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# noise_words = []
-# ### Creating a python object of the class CountVectorizer
-# tfidf_counts = TfidfVectorizer(tokenizer= word_tokenize, # type of tokenization
-#                                stop_words=noise_words, # List of stopwords
-#                                ngram_range=(1,1)) # number of n-grams
-
-# tfidf_data = tfidf_counts.fit_transform(data['reviews_text_new'])
-
-# X_train_tfidf, X_test_tfidf, y_train_tfidf, y_test_tfidf = train_test_split(tfidf_data,
-#                                                                             data['Overall Sentiment'],
-#                                                                             test_size = 0.2,
-#                                                                             random_state = 0)
-
-# ### Setting up the model class
-# lr_model_tf_idf = LogisticRegression()
-
-# ## Training the model 
-# lr_model_tf_idf.fit(X_train_tfidf,y_train_tfidf)
-
-# ## Prediciting the results
-# test_pred_lr_all = lr_model_tf_idf.predict(X_test_tfidf)
-
-# ## Calculate key performance metrics
-
-# # Print a classification report
-# print(classification_report(y_test_tfidf,test_pred_lr_all))
+print(classification_report(y_test_tfidf,test_pred_lr_all))
